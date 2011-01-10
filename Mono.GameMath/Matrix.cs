b687a5933626f6c291d05c94e3a03d9bb822cf2f
XTZGZoReX <xtzgzorex@gmail.com>
2011-01-10 04:11:01 +0100

    Optimize Matrix.ToString().

--- Mono.GameMath/Matrix.cs
+++ Mono.GameMath/Matrix.cs
@@ -1142,11 +1142,17 @@ namespace Mono.GameMath
 		
 		public override string ToString ()
 		{
-			return string.Format("{{{0} {1} {2} {3}}}",
-			                     string.Format("{{M11:{0} M12:{1} M13:{2} M14:{3}}} ", M11, M12, M13, M14),
-			                     string.Format("{{M21:{0} M22:{1} M23:{2} M24:{3}}} ", M21, M22, M23, M24),
-			                     string.Format("{{M31:{0} M32:{1} M33:{2} M34:{3}}} ", M31, M32, M33, M34),
-			                     string.Format("{{M41:{0} M42:{1} M43:{2} M44:{3}}}",  M41, M42, M43, M44));
+			return string.Format(
+				"{{" +
+				"{{M11:{0} M12:{1} M13:{2} M14:{3}}} " +
+				"{{M21:{4} M22:{5} M23:{6} M24:{7}}} " +
+				"{{M31:{8} M32:{9} M33:{10} M34:{11}}} " +
+				"{{M41:{12} M42:{13} M43:{14} M44:{15}}}" +
+				"}}",
+				M11, M12, M13, M14,
+				M21, M22, M23, M24,
+				M31, M32, M33, M34,
+				M41, M42, M43, M44);
 		}
 	}
 }
